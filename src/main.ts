bebe80db5ae09f8880220dc825bf540d67627640
chenzhigang <979703986@qq.com>
2023-01-09 18:05:09 +0800

    chore: store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import store from '@/store'
 
 const app = createApp(App)
 
@@ -8,4 +9,4 @@ const app = createApp(App)
   import.meta.glob('./plugins/*.ts', { eager: true }),
 )
 
-app.use(router).mount('#app')
+app.use(router).use(store).mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = [{ path: '/:catchAll(.*)*', name: '404', redirect: '/' }]
+const routes: RouteRecordRaw[] = [
+  { path: '/', component: () => import('src/views/home/index.vue') },
+  { path: '/:catchAll(.*)*', name: '404', redirect: '/' },
+]
 
 export default createRouter({
   history: createWebHashHistory(),
