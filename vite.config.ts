7c075b3f3645a903a2c7ecf624b29be44a53d5bb
chenzhigang <979703986@qq.com>
2023-01-09 10:43:08 +0800

    chore: package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+const resolve = dir => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      resolvers: [
+        ElementPlusResolver()
+      ],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver()
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      'src': resolve('src'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {}
+    }
+  },
 })
