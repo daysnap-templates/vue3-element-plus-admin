5ba2a6bab7dff310a804311007a984bb2efbdb29
chenzhigang <979703986@qq.com>
2023-07-06 15:31:19 +0800

    chore: clean project

--- src/layout/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './useMenuCollapse'
-export * from './useVisitedTags'
--- src/views/404/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>404页面</div>
-</template>
-
-<script lang="ts" setup></script>
-
-<style lang="scss" scoped>
-@import 'src/assets/scss/define';
-</style>
