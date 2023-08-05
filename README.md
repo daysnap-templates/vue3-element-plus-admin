a41c884b273cf3f6651b15750445dbdbd9b307b2
chenzhigang <979703986@qq.com>
2023-08-05 23:02:51 +0800

    chore: 项目依赖

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # .
 
+## 搭建
+
+```
+# 初始化项目
+npm init vue@latest
+
+# 安装依赖包
+npm install element-plus -S
+
+npm install unplugin-vue-components unplugin-auto-import -D
+
+npm install sass -D
+
+npm install pinia-plugin-persistedstate -S
+
+npm install axios -S
+
+npm install nprogress -S
+
+npm install @daysnap/define-sass -D
+
+npm install @daysnap/utils -S
+```
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,32 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    // https://element-plus.org/zh-CN/guide/quickstart.html#%E6%8C%89%E9%9C%80%E5%AF%BC%E5%85%A5
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'typings/auto-imports.d.ts',
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      dts: 'typings/components.d.ts',
+      // extensions: ['ts', 'jsx', 'tsx', 'js', 'vue'],
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
