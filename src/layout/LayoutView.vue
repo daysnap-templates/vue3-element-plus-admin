22edc867031b9edda4f5207c6acf5b2067e073c0
chenzhigang <979703986@qq.com>
2023-08-06 23:21:53 +0800

    chore: 更新页面

--- src/layout/LayoutView.vue
+++ src/layout/LayoutView.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>LayoutView</div>
+  <div class="layout-wrap">
+    <div class="layout-mask"></div>
+    <Sidebar />
+    <div class="layout-inner">
+      <div class="layout-header">
+        <Navbar />
+        <Tags />
+      </div>
+      <div class="layout-content">
+        <RouterView />
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { Navbar, Sidebar, Tags } from './components'
+</script>
 
 <style lang="scss" scoped>
   @import '@/assets/scss/define.scss';
+  .layout-wrap {
+  }
+  .layout-mask {
+  }
+  .layout-inner {
+    min-height: 100vh;
+    margin-left: 230px;
+  }
+  .layout-header {
+  }
+  .layout-content {
+  }
 </style>
--- src/layout/components/sidebar/Sidebar.vue
+++ src/layout/components/sidebar/Sidebar.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <div class="sidebar"></div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="scss" scoped>
   @import '@/assets/scss/define.scss';
+  .sidebar {
+    @extend %pf;
+    @extend %l0;
+    @extend %t0;
+    @extend %b0;
+    z-index: 9;
+    width: 230px;
+    background-color: red;
+  }
 </style>
